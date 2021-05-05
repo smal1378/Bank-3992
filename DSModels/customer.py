16a5de3d3a3defd7df51964419949c20f993cc6e
Bassam <bassamnazemi.bn@gmail.com>
2021-05-06 00:18:23 +0430

    Add user and customer class

---
+++ DSModels/customer.py
@@ -0,0 +1,13 @@
+from userClass import User
+
+class Customer(User):
+    cash = 0
+
+    def moneyReception(self): #daryaft vajh
+        pass
+    def moneyRemitment(self): #enteghal vajh
+        pass
+    def showCash(self): #moshahede mojudi
+        pass
+    def recentTransaction(self) #tarakonesh ha
+        pass
