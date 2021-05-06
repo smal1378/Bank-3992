7b6545f1edbab71688bccbf6e3f3b473d0580132
hamed <hamed14410158@gmail.com>
2021-05-06 23:06:09 +0430

    add new methods to DS.py

--- DS.py
+++ DS.py
@@ -4,4 +4,86 @@
 
 
 class Core:
-    pass
+    def __init__(self):
+        pass
+
+    def check_login(self, username, password):
+        pass
+
+    def funds_transfer(self, account_1, account_2):
+        pass
+
+    def get_balance(self):
+        pass
+
+    def deposit(self):
+        pass
+
+    def get_account_number(self):
+        pass
+
+    def withdraw(self):
+        pass
+
+    def transfer_history(self):
+        pass
+
+    def all_accounts(self):
+        pass
+
+    def change_username(self):
+        pass
+
+    def change_password(self):
+        pass
+
+    def create_account(self):
+        pass
+
+    def create_user(self):
+        pass
+
+    def search_user(self):
+        pass
+
+    def add_account(self):
+        pass
+
+    def del_account(self):
+        pass
+
+    def user_detail(self):
+        pass
+
+    def user_detail_change(self):
+        pass
+
+    def create_branch(self):
+        pass
+
+    def del_branch(self):
+        pass
+
+    def get_branch_employee(self):
+        pass
+
+    def get_all_employee(self):
+        pass
+
+    def get_all_customer(self):
+        pass
+
+    def get_all_user(self):
+        pass
+
+    def employees_payment(self):
+        pass
+
+    def account_fee(self):
+        pass
+
+    def transfer_fee(self):
+        pass
+
+    def get_bank_account_balance(self):
+        pass
