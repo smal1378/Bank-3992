3e911fd34c0a72e6956e3b634bb0cc834520467c
bassamPy <bassampython@gmail.com>
2021-05-12 23:45:46 +0430

    Add methods for customer class

--- DSModels/customer.py
+++ DSModels/customer.py
@@ -1,13 +1,18 @@
-from userClass import User
+from user import User
+
 
 class Customer(User):
+
     cash = 0
 
-    def moneyReception(self): #daryaft vajh
-        pass
-    def moneyRemitment(self): #enteghal vajh
-        pass
-    def showCash(self): #moshahede mojudi
-        pass
-    def recentTransaction(self) #tarakonesh ha
+    def money_reception(self, amount): #money_reception: daryaft vajh     amout: mablagh
+        self.cash -= amount
+
+    def money_remitment(self, amount, card_number): #money_reminent: entefgal vajh     card_number: shomare card
+        self.cash -= amount
+
+    def show_cash(self): #show_cash: moshahede mojudi
+        return self.cash
+
+    def recent_transaction(self): #recent_transaction: tarakonesh haye akhir
         pass
