039fa7ab8ee32aab1d6c378367f9c7080f0834b7
bassamPy <bassampython@gmail.com>
2021-05-13 15:00:42 +0430

    Update names and use PEP, improve Customer class

--- DSModels/customer.py
+++ DSModels/customer.py
@@ -3,16 +3,23 @@ from user import User
 
 class Customer(User):
 
-    cash = 0
+    def __init__(self, first_name, last_name, username, password, national_code, address, access_level, balance):
+        super().__init__(self, first_name, last_name, username, password, national_code, address, access_level)
+        self.balance = balance
 
     def money_reception(self, amount): #money_reception: daryaft vajh     amout: mablagh
-        self.cash -= amount
+        while True:
+            if(amount <= self.balance):
+                self.balance -= amount
+                break
+            else:
+                #return 'There is not enough balance!'
 
     def money_remitment(self, amount, card_number): #money_reminent: entefgal vajh     card_number: shomare card
-        self.cash -= amount
+        self.balance -= amount
 
     def show_cash(self): #show_cash: moshahede mojudi
-        return self.cash
+        return (self.balance)
 
     def recent_transaction(self): #recent_transaction: tarakonesh haye akhir
         pass
