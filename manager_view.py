f7194a90f5a899f2c1b9cd45bd7254aec1ae4361
sia <83309822+callmesia@users.noreply.github.com>
2021-05-26 09:47:08 +0430

    new changes to files based on manager callbacks

--- manager_view.py
+++ manager_view.py
@@ -1,6 +1,7 @@
 from gui_configuration import Tk, Button, Frame
-from edit_view import EditView
+from edit_user_view import EditUserView
 from edit_me_view import EditMeView
+from add_user_view import AddUserView
 import tkinter as tk 
 from tkinter import ttk
 
@@ -9,24 +10,25 @@ class ManagerView(Tk):
     '''
     This class creates manager panel.
     Access to: 
-        edit_view panel
-        edit_me panel
-        add_user panel
+        edit_user_view panel
+        edit_me_view panel
+        add_user_view panel
     '''
         
     def __init__(self, name, username, list_employees,
                  callback1 = None, callback2 = None,
                  callback3 = None, callback4 = None,
-                 callback5 = None):
+                 callback5 = None, callback6 = None):
         super().__init__()
         self.name = name
         self.username = username
         self.list_employees = list_employees
         self.callback1 = callback1  # add_employee()
-        self.callback2 = callback2  # remove_employee()
-        self.callback3 = callback3  # change_employee
+        self.callback2 = callback2  # del_employee()
+        self.callback3 = callback3  # change_employee()
         self.callback4 = callback4  # open_employee_panel()
         self.callback5 = callback5  # search()
+        self.callback6 = callback6  # set_my_info()
         self.title(f"Panel manager: {self.name}")
         self.bind("<Configure>", self.auto_geometry)
         #self.icon (may add it)
@@ -88,7 +90,6 @@ class ManagerView(Tk):
                                         height = 15)
         self.users_treev.grid(row = 0, column = 0)
         self.users_treev['columns'] = ("1", "2") 
-        self.numeric_columns = ("2",) # used to sort columns properly
         self.users_treev["show"] = "headings"
         self.users_treev.column("1", width = 140, anchor ='c')
         self.users_treev.column("2", width = 140, anchor ='c')
@@ -96,7 +97,7 @@ class ManagerView(Tk):
         self.users_treev.heading("1", text ="Name",
                                  command = lambda: \
                                  self.treev_cl_sort("1", False))
-        self.users_treev.heading("2", text ="ID",
+        self.users_treev.heading("2", text ="Username",
                                  command = lambda: \
                                  self.treev_cl_sort("2", False))
 
@@ -120,7 +121,6 @@ class ManagerView(Tk):
         for user in self.list_employees:
             self.users_treev.insert("", "end", values = user)
         self.users_treev.bind("<Double-1>", self.onDoubleClick)
-        
     def onDoubleClick(self, event):
         '''
         assigning a double-click function to treev to open their panel.
@@ -129,46 +129,56 @@ class ManagerView(Tk):
         user = self.users_treev.selection()
         if user == ():  # double click on headings or any other empty space
             return None
-        name, id_ = self.users_treev.item(user)["values"] # = (name, id)
-        # if self.callback4:
-        #     self.callback4() 
+        name, username = self.users_treev.item(user)["values"] # =(name, user)
+        if self.callback4:
+            self.callback4(username) 
         self.users_treev.selection_remove(user[0]) # clear selection
         
     def edit_me(self):
-        panel = EditMeView(self.username, self.tab_edit_me, self.callback3, 
-                                                            self.close_tab)
-        print(self.tabs.index(self.tabs.tabs()[0]))
+        children = self.tab_edit_me.winfo_children()
+        if children != []:  # if a edit_view panel is open then ignore.
+            return None
+        panel = EditMeView(self.username, self.tab_edit_me, self.callback6, 
+                           self.close_tab, self.msg_err)
+
         self.tabs.add(self.tab_edit_me, text = "Edit-me menu")
         
     def add_user(self):
-        #panel = AddView(self.tab_add_user, self.callback3, self.close_tab)
-        #self.tabs.add(self.tab_add_user, text = "Add-user")
-        # if self.callback1:
-        #     self.callback1(name, username, password, id_)
-        print("add_user test")
+        children = self.tab_add_user.winfo_children()
+        if children != []:  # if a edit_view panel is open then ignore.
+            return None
+        panel = AddUserView(self.tab_add_user, self.callback1,
+                            self.close_tab, self.msg_err)
+        self.tabs.add(self.tab_add_user, text = "Add-user")
         
     def del_user(self):
         selected = self.users_treev.focus()  # returns selected item ID
         if selected == "":  # nothig selected, but btn is ckicked
             return None
-        name, id_ = self.users_treev.item(selected, "values") # (name, id)
+        name, username = self.users_treev.item(selected, "values")
         self.users_treev.delete(selected)
         if self.callback2:
-            self.callback2(id_)
+            self.callback2(username, self.msg_err)
         # update self.list_emplyees
-        print("test del btn")
         
     def edit_user(self):
+        '''
+        first delete any opened tab for editing and then open new tab for edit
+        '''
+        children = self.tab_edit_user.winfo_children()
+        if children != []:
+            for widget in children:  # wiget = last EditUserView()
+                widget.destroy()
         selected = self.users_treev.focus()
         if selected == "":  # nothig selected, but btn is ckicked
             return None
-        name, id_ = self.users_treev.item(selected, "values")
-        panel = EditView(name, id_, self.tab_edit_user, self.callback3, 
-                                                        self.close_tab)
-        self.tabs.add(self.tab_edit_user, text = "Edit-user")
+        name, username = self.users_treev.item(selected, "values")
+        panel = EditUserView(name, username, self.tab_edit_user,
+                             self.callback3, self.close_tab, self.msg_err)
+        self.tabs.add(self.tab_edit_user, text = f"Edit {name}")
         # update self.list_employees (update by order of main.py).
         # update treeview. call refresh_treev after list update.
-        self.users_treev.selection_remove(selected) # clear selection
+        self.users_treev.selection_remove(selected) 
         
     def refresh_treev(self):
         '''simply deletes the entire treev and inserts items again'''
@@ -177,7 +187,11 @@ class ManagerView(Tk):
         for user in self.list_employees:
             self.users_treev.insert("", "end", values = user)
         
-    def search_treev(self):  # key could be anything
+    def search_treev(self):
+        '''
+        gets anythin in self.search_bar and send to main.py.
+        self.msg_err if nothing was found
+        '''
         text = self.search_bar.get()
         if self.callback5:
             matched_items = self.callback5(text)
@@ -186,17 +200,12 @@ class ManagerView(Tk):
             for item in matched_items:
                 self.users_treev.insert("", "end", values = item)
         else:
-            # messegebox.showalart()
-            print("nothing found") # test
+            self.msg_err("Error", "User not found")
    
     def treev_cl_sort(self, col, reverse):
         ''' click on each heading to sort by the values of it.'''
         
-        if col in self.numeric_columns:
-            users = [(int(self.users_treev.set(id_, col)), id_) \
-                      for id_ in self.users_treev.get_children()]
-        else:
-            users = [(self.users_treev.set(id_, col), id_) \
+        users = [(self.users_treev.set(id_, col), id_) \
                       for id_ in self.users_treev.get_children()]
         # users = [(value 1, id-row 1), (value 2, id-row 2), ...]
         users.sort(reverse = reverse)  # sorted by values. not row-id !
@@ -228,7 +237,18 @@ class ManagerView(Tk):
         place_y = int(screen_h / 2) - int(tk_h / 2)
         if tk_w != 1 and tk_h != 1:
             self.geometry(f"{tk_w}x{tk_h}+{place_x}+{place_y}")
-            self.unbind("<Configure>")  # need it here, trust me.    
+            self.unbind("<Configure>")  # need it here, trust me.   
+            
+    def run(self):
+        self.mainloop()
+        
+    def close(self):
+        self.destroy()
+    
+    def msg_err(self, title = None, msg = None, master = None):
+        if master is None:
+            master = self
+        tk.messagebox.showerror(title, msg, parent = master)
 
 
 if __name__ == "__main__":
@@ -236,5 +256,5 @@ if __name__ == "__main__":
     lst = [(f"user {i + 1}", random.randint(10**11, (10**12) - 1)) \
            for i in range(20)]
     panel = ManagerView("siamak", "siamak_sdi", lst)
-    panel.mainloop() 
+    panel.run() 
     
